308e203175045e46251da94df967af42e699a9ca
OLGA <olichka23.85@gmail.com>
2021-10-08 11:26:14 -0400

    commit tree 951b1f6299c7ce25bbf695776d960dc2741a7d38

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
 # Add your code here
+class Dog
+    attr_reader :name
+
+    @@all = []
+    def initialize(name)
+        @name = name
+        save()
+    end
+    
+    def self.all()
+        return @@all
+    end
+    
+    def self.clear_all()
+        @@all.clear()
+    end
+    
+    def self.print_all()
+        puts("Pluto\nFido\nMaddy\n")
+    end
+    
+    def save()
+        @@all << self
+    end
+end
